8dbf235b88d93134d2c7256f68b0cc357e14ed51
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-03-12 12:23:07 +0100

    Función: Contar información

--- functions.py
+++ functions.py
@@ -17,3 +17,19 @@ def listar_informacion(diccionario):
         if biblioteca_input == datos['biblioteca']:
             for libros in datos['libros']:
                 print(f"    - {libros['titulo']}, escrito por {libros['autor']}. De {libros['genero']} y publicado en el {libros['año_publicacion']}")
+
+# Consulta 2: Contar información
+def contar_informacion(diccionario):
+    print("Las bibliotecas disponibles son:")
+    for datos in diccionario:
+        print(f"    - {datos['biblioteca']}")
+    biblioteca_input = input("Introduce una biblioteca: ")
+    libros_count = 0
+    prestamos_count = 0
+    for datos in diccionario:
+        if biblioteca_input == datos['biblioteca']:
+            for libros in datos['libros']:
+                libros_count += 1
+                for prestamos in libros['prestamos']:
+                    prestamos_count += 1
+    print(f"En la {biblioteca_input} hay un total de {libros_count} libros y se han realizado un total de {prestamos_count} préstamos.")
