e6ce2bc0fdd1837ab8cf4884db74cd935be3efe1
f1rul4yx <vargasgomezderequenadiego@gmail.com>
2025-03-12 11:55:41 +0100

    Función: Listar información

--- functions.py
+++ functions.py
@@ -5,3 +5,15 @@ def leer_fichero(archivo):
     with open(archivo, 'rt') as archivo:
         archivo = json.load(archivo)
     return archivo
+
+# Consulta 1: Listar información
+def listar_informacion(diccionario):
+    print("Las bibliotecas disponibles son:")
+    for datos in diccionario:
+        print(f"    - {datos['biblioteca']}")
+    biblioteca_input = input("Introduce una biblioteca: ")
+    print(f"Los libros que hay en la {biblioteca_input} son:")
+    for datos in diccionario:
+        if biblioteca_input == datos['biblioteca']:
+            for libros in datos['libros']:
+                print(f"    - {libros['titulo']}, escrito por {libros['autor']}. De {libros['genero']} y publicado en el {libros['año_publicacion']}")
